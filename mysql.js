a30ae3ec85b10be67c7c33e35ddf8879133b62f9
VhrandomUV <brandon.villalobos@alumnos.uv.cl>
2023-07-02 18:55:25 -0400

    node sql

--- mysql.js
+++ mysql.js
@@ -8,21 +8,21 @@ const conection = mysql.createConnection({
     database:'prueba'
 })
 
-function getTemperatura(min, max) {
+function temperatura_sql(min, max) {
     min = Math.ceil(25);
     max = Math.floor(32);
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  function hora(){
+  function hora_sql(){
     const d = new Date()
     var horas = d.toLocaleTimeString()
     formated = horas.replaceAll(':','')
     return formated
 }
 
-var h = hora()
-var t = getTemperatura()
+var h = hora_sql()
+var t = temperatura_sql()
 
 conection.connect( (err) => {
     if(err) throw err
