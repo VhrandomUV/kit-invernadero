83db15923199a098449dc68b5561506f05d9dc93
VhrandomUV <brandon.villalobos@alumnos.uv.cl>
2023-07-02 20:35:23 -0400

    nope

--- mysql.js
+++ mysql.js
@@ -21,6 +21,23 @@ function temperatura_sql(min, max) {
     return formated
 }
 
+
+function fecha() {
+    var d = new Date();
+    var fecha = d.toLocaleDateString();
+    formated = fecha.replaceAll('-','/')
+    return formated
+}
+
+function humendad_sql(min, max) {
+    min = Math.ceil(55);
+    max = Math.floor(70);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+var f = fecha() 
+
+var hum = humendad_sql()
 var h = hora_sql()
 var t = temperatura_sql()
 
@@ -42,15 +59,11 @@ conection.query('SELECT * FROM eventos', (err, rows) => {
 
 })
 
-
-function prueba(){
-    conection.query(`INSERT INTO eventos (hora, medicion) VALUES(${h}, ${t})` , (err, rows) => {
-        if(err) throw err
-        
-        document.getElementById('elementos').innerHTML = h;
-    })
-    
-}
+conection.query(`INSERT INTO humedad (fecha, humedad, temperatura, hora) VALUES('${f}' ,${hum}, ${t}, ${h})` , (err, rows) => {
+    if(err) throw err
+    console.log('la conexion funciona') 
+    console.log(f)
+})
 
 
 
